8a1ce39dd580c0e6ead2d3608f5402c2b1c0894b
William Hallatt <goblincoding@gmail.com>
2013-11-08 17:24:33 +0200

    Added qtbuild comments

--- qtbuild.sh
+++ qtbuild.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+
+# Script for buliding Qt 5.x.
+#
+# Clean the repo VERY thoroughly.
 git submodule foreach --recursive "git clean -dfx"
+
+# Pull in all changes.
 git pull
 git submodule sync
 git submodule update --recursive
+
+# Reconfigure Qt.
 ./configure -developer-build -opensource -nomake examples -nomake tests -no-gtkstyle -confirm-license
+
+# Make the magic happen.
 make -j 4
 make docs
 
